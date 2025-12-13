c0ddfaac3763595efb826ff93d0ea242ff749d1f
Sebastian906 <elsebas1912@gmail.com>
2025-12-13 18:17:53 -0500

    Creación de páginas correspondientes en la interfaz y enrutamiento en App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import HomePage from './pages/HomePage'
+import CreatePage from './pages/CreatePage'
+import { NoteDetailsPage } from './pages/NoteDetailsPage'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/create" element={<CreatePage />} />
+        <Route path="/note/:id" element={<NoteDetailsPage />} />
+      </Routes>
+    </div>
   )
 }
 
